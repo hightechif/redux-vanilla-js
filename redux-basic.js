7cb60cac72cb9362c217fabc1021ea475c78b436
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-04-19 21:07:55 +0700

    main app

--- redux-basic.js
+++ redux-basic.js
@@ -9,28 +9,53 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   console.log(`ACTION dipanggil (dispatch [${action.type}])`);
   
-  if (action.type === "INCREMENT_COUNTER") {
-    const newState = {
-      ...state,
-      counter: state.counter + 1
-    }
-    return newState
-  }
+  // Cara 1
+  // if (action.type === "INCREMENT_COUNTER") {
+  //   const newState = {
+  //     ...state,
+  //     counter: state.counter + 1
+  //   }
+  //   return newState
+  // }
 
-  if (action.type === "ADDING_COUNTER") {
-    const newState = {
-      ...state,
-      counter: state.counter + action.payload.value
-    }
-    return newState
-  }
+  // if (action.type === "ADDING_COUNTER") {
+  //   const newState = {
+  //     ...state,
+  //     counter: state.counter + action.payload.value
+  //   }
+  //   return newState
+  // }
 
-  return state;
+  // return state;
+
+  // Cara 2
+  let newState ;
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      newState = {
+        ...state,
+        counter: state.counter + 1
+      }
+      return newState
+    case 'ADDING_COUNTER':
+      newState = {
+        ...state,
+        counter: state.counter + action.payload.value
+      }
+      return newState
+    default:
+      return state;
+  }
 }
 
 // Store
 const store = createStore(rootReducer)
 console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log("[Subscription] ke-trigger", store.getState());
+})
 
 // Action (Dispatching Action)
 store.dispatch({              // Setiap ada dispatch maka reducer pasti dijalankan
@@ -40,7 +65,7 @@ store.dispatch({              // Setiap ada dispatch maka reducer pasti dijalank
 store.dispatch({
   type: "ADDING_COUNTER",
   payload: {
-    value: 5
+    value: 10
   }
 })
 
